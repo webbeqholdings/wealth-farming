4556e8722a0464a0f574359d04bb36d530c1d0f9
HDNghia <huynhdangnghia68@gmail.com>
2024-11-08 09:31:25 +0700

    update migrate

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20241106_030511 from './20241106_030511';
+import * as migration_20241108_022637_address from './20241108_022637_address';
 
 export const migrations = [
   {
     up: migration_20241106_030511.up,
     down: migration_20241106_030511.down,
-    name: '20241106_030511'
+    name: '20241106_030511',
+  },
+  {
+    up: migration_20241108_022637_address.up,
+    down: migration_20241108_022637_address.down,
+    name: '20241108_022637_address'
   },
 ];
