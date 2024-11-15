f9e3b3c17837da9d4ad34dc27d371e879be9ba73
HDNghia <huynhdangnghia68@gmail.com>
2024-11-15 10:13:45 +0700

    update collection user

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20241110_073737_init from './20241110_073737_init';
 import * as migration_20241111_021949_update_global from './20241111_021949_update_global';
+import * as migration_20241115_030718_add_new_column_to_users_table from './20241115_030718_add_new_column_to_users_table';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20241111_021949_update_global.up,
     down: migration_20241111_021949_update_global.down,
-    name: '20241111_021949_update_global'
+    name: '20241111_021949_update_global',
+  },
+  {
+    up: migration_20241115_030718_add_new_column_to_users_table.up,
+    down: migration_20241115_030718_add_new_column_to_users_table.down,
+    name: '20241115_030718_add_new_column_to_users_table'
   },
 ];
