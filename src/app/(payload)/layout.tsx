4b91d965c38ed3b0b1461f8e16b40c5f49172edb
HDNghia <huynhdangnghia68@gmail.com>
2024-11-27 09:30:52 +0700

    update payload/layout.tsx

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -1,9 +1,9 @@
 /* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
 /* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
-import config from '@payload-config'
+import configPromise from '@payload-config'
 import '@payloadcms/next/css'
-import type { ServerFunctionClient } from 'payload'
-import { handleServerFunctions, RootLayout } from '@payloadcms/next/layouts'
+import { RootLayout } from '@payloadcms/next/layouts'
+// import '@payloadcms/ui/styles.css' // Uncomment this line if `@payloadcms/ui` in `tsconfig.json` points to `/ui/dist` instead of `/ui/src`
 import React from 'react'
 
 import { importMap } from './admin/importMap.js'
@@ -13,17 +13,8 @@ type Args = {
   children: React.ReactNode
 }
 
-const serverFunction: ServerFunctionClient = async function (args) {
-  'use server'
-  return handleServerFunctions({
-    ...args,
-    config,
-    importMap,
-  })
-}
-
 const Layout = ({ children }: Args) => (
-  <RootLayout config={config} importMap={importMap} serverFunction={serverFunction}>
+  <RootLayout config={configPromise} importMap={importMap}>
     {children}
   </RootLayout>
 )
