610dc3306fd0b4f7198eb0fdbe379b7ef8a4fe14
banhmioopla <qbingking@gmail.com>
2024-12-02 18:23:53 +0700

    defaultTheme is dark

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -78,7 +78,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         >
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
