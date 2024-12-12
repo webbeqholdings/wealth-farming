a8e9b898166c2cf4bb8bdba7c7cc3c59bbcf3933
HDNghia <huynhdangnghia68@gmail.com>
2024-12-12 08:53:51 +0700

    add migrate create seo

---
+++ src/migrations/20241212_015324_create_seo_collection.ts
@@ -0,0 +1,40 @@
+import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
+
+export async function up({ payload, req }: MigrateUpArgs): Promise<void> {
+  await payload.db.drizzle.execute(sql`
+   CREATE TABLE IF NOT EXISTS "seo" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"meta_title" varchar,
+  	"meta_description" varchar,
+  	"meta_image_id" integer,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
+  );
+  
+  ALTER TABLE "payload_locked_documents_rels" ADD COLUMN "seo_id" integer;
+  DO $$ BEGIN
+   ALTER TABLE "seo" ADD CONSTRAINT "seo_meta_image_id_media_id_fk" FOREIGN KEY ("meta_image_id") REFERENCES "public"."media"("id") ON DELETE set null ON UPDATE no action;
+  EXCEPTION
+   WHEN duplicate_object THEN null;
+  END $$;
+  
+  CREATE INDEX IF NOT EXISTS "seo_meta_meta_image_idx" ON "seo" USING btree ("meta_image_id");
+  CREATE INDEX IF NOT EXISTS "seo_updated_at_idx" ON "seo" USING btree ("updated_at");
+  CREATE INDEX IF NOT EXISTS "seo_created_at_idx" ON "seo" USING btree ("created_at");
+  DO $$ BEGIN
+   ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_seo_fk" FOREIGN KEY ("seo_id") REFERENCES "public"."seo"("id") ON DELETE cascade ON UPDATE no action;
+  EXCEPTION
+   WHEN duplicate_object THEN null;
+  END $$;
+  
+  CREATE INDEX IF NOT EXISTS "payload_locked_documents_rels_seo_id_idx" ON "payload_locked_documents_rels" USING btree ("seo_id");`)
+}
+
+export async function down({ payload, req }: MigrateDownArgs): Promise<void> {
+  await payload.db.drizzle.execute(sql`
+   DROP TABLE "seo";
+  ALTER TABLE "payload_locked_documents_rels" DROP CONSTRAINT "payload_locked_documents_rels_seo_fk";
+  
+  DROP INDEX IF EXISTS "payload_locked_documents_rels_seo_id_idx";
+  ALTER TABLE "payload_locked_documents_rels" DROP COLUMN IF EXISTS "seo_id";`)
+}
--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,8 +1,7 @@
 import * as migration_20241205_104255_create_telegram_table from './20241205_104255_create_telegram_table';
 import * as migration_20241210_041714_add_notification_and_economic_calendar from './20241210_041714_add_notification_and_economic_calendar';
 import * as migration_20241210_091236_add_field_product_overview from './20241210_091236_add_field_product_overview';
-import * as migration_20241211_044238_create_pages_collection from './20241211_044238_create_pages_collection';
-import * as migration_20241212_013944_change_name_pages from './20241212_013944_change_name_pages';
+import * as migration_20241212_015324_create_seo_collection from './20241212_015324_create_seo_collection';
 
 export const migrations = [
   {
@@ -21,13 +20,8 @@ export const migrations = [
     name: '20241210_091236_add_field_product_overview',
   },
   {
-    up: migration_20241211_044238_create_pages_collection.up,
-    down: migration_20241211_044238_create_pages_collection.down,
-    name: '20241211_044238_create_pages_collection',
-  },
-  {
-    up: migration_20241212_013944_change_name_pages.up,
-    down: migration_20241212_013944_change_name_pages.down,
-    name: '20241212_013944_change_name_pages'
+    up: migration_20241212_015324_create_seo_collection.up,
+    down: migration_20241212_015324_create_seo_collection.down,
+    name: '20241212_015324_create_seo_collection'
   },
 ];
