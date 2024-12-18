eb4a9bcb2f0fb0835a94e4891fbbb29348fdd0c0
HDNghia <huynhdangnghia68@gmail.com>
2024-12-18 09:08:21 +0700

    add auth access for method delete telegram

--- src/collections/Telegram.ts
+++ src/collections/Telegram.ts
@@ -6,7 +6,23 @@ const Telegram: CollectionConfig = {
     read: () => true, // Publicly readable
     create: ({ req: { user } }) => user?.role === 'admin', // Only admins can create
     update: ({ req: { user } }) => user?.role === 'admin', // Only admins can update
-    delete: ({ req: { user }, id }) => user?.role === 'admin' || user?.id === id, // Only admins or user can delete
+    delete: ({ req: { user, query } }) => {
+      if(user?.role == 'admin'){
+        return true;
+      }
+      // Extract the ID from the query
+      if (typeof query.where === 'object' && 'id' in query.where && typeof query.where.id === 'object' && 'equals' in query.where.id) {
+        const id = Number(query?.where?.id?.equals);
+        if (typeof user.telegram === 'object' && 'id' in user.telegram) {
+          if(id !== Number(user.telegram.id)){
+            return false
+          }
+          return true
+        }
+      }
+
+      return false
+    },
   },
   fields: [
     {
