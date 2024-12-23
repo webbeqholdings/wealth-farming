272551e267dc3aa67ab129caf160bde0e2c1f131
HDNghia <huynhdangnghia68@gmail.com>
2024-12-23 19:29:53 +0700

    add loading to referrals

--- src/app/(app)/account/referral/page.tsx
+++ src/app/(app)/account/referral/page.tsx
@@ -73,7 +73,7 @@ export default function ReferralPage() {
     }
 
     fetchReferralData()
-  }, [])
+  }, [loading])
 
   const copyReferralLink = () => {
     navigator.clipboard.writeText(referralLink)
