c260caf2217c0b11bd2e90250c9632cf092a3d98
caokhanhquynh <kqc@sfu.ca>
2025-02-11 11:29:56 +0700

    update minimal hero

--- src/components/page-home/MinimalHero.tsx
+++ src/components/page-home/MinimalHero.tsx
@@ -4,6 +4,8 @@ import React, { useEffect, useRef } from 'react'
 import { Button } from '@/components/ui/button'
 // import { GlobeGithub } from '@/components/GlobeGithub'
 import { useTranslation } from 'react-i18next';
+import WorldMap from "@/components/ui/world-map";
+import { motion } from "motion/react";
 
 import {
   PageHeaderHeading,
@@ -34,7 +36,52 @@ export function MinimalHero() {
             </PageHeader>
           </div>
           <div className="mx-auto lg:mx-0 aspect-video overflow-hidden rounded-xl order-1 lg:order-2">
-            {/* <GlobeGithub /> */}
+            <div className="">
+              <WorldMap
+                dots={[
+                  {
+                    start: {
+                      lat: 64.2008,
+                      lng: -149.4937,
+                    }, // Alaska (Fairbanks)
+                    end: {
+                      lat: 34.0522,
+                      lng: -118.2437,
+                    }, // Los Angeles
+                  },
+                  {
+                    start: {
+                      lat: 64.2008,
+                      lng: -149.4937,
+                    }, // Alaska (Fairbanks)
+                    end: {
+                      lat: 34.0522,
+                      lng: -118.2437,
+                    }, // Los Angeles
+                  },
+                  {
+                    start: { lat: 64.2008, lng: -149.4937 }, // Alaska (Fairbanks)
+                    end: { lat: -15.7975, lng: -47.8919 }, // Brazil (Brasília)
+                  },
+                  {
+                    start: { lat: -15.7975, lng: -47.8919 }, // Brazil (Brasília)
+                    end: { lat: 38.7223, lng: -9.1393 }, // Lisbon
+                  },
+                  {
+                    start: { lat: 51.5074, lng: -0.1278 }, // London
+                    end: { lat: 28.6139, lng: 77.209 }, // New Delhi
+                  },
+                  {
+                    start: { lat: 28.6139, lng: 77.209 }, // New Delhi
+                    end: { lat: 43.1332, lng: 131.9113 }, // Vladivostok
+                  },
+                  {
+                    start: { lat: 28.6139, lng: 77.209 }, // New Delhi
+                    end: { lat: -1.2921, lng: 36.8219 }, // Nairobi
+                  },
+                ]}
+              />
+            </div>
           </div>
         </div>
       </div>
